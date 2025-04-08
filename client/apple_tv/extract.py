a02273c41ba81285c5d4c774de05958db0d99fd7
matthieumontaigu <matthieu.montaigu@icloud.com>
2025-04-08 17:37:31 +0200

    Add extract_artworks function to retrieve logo and background images from a given URL

---
+++ client/apple_tv/extract.py
@@ -0,0 +1,73 @@
+import re
+
+from bs4 import BeautifulSoup
+from utils.parsing import parse_html
+from utils.requests_utils import get_request
+
+
+def extract_artworks(url: str) -> dict[str, str]:
+    response = get_request(url)
+    response.raise_for_status()
+    parsed_page = parse_html(response.text)
+
+    logo = get_logo_url(parsed_page)
+    background = get_background_url(parsed_page)
+
+    artworks = {}
+    if logo is not None:
+        artworks["logo_url"] = logo
+
+    if background is not None:
+        artworks["background_url"] = background
+    return artworks
+
+
+def get_logo_url(page: BeautifulSoup) -> str | None:
+    pictures = page.find_all("picture")
+    logo_picture = None
+    for picture in pictures:
+        img = picture.find("img", class_="product-header__image-logo__image")
+        if img:
+            logo_picture = picture
+            break
+
+    if not logo_picture:
+        return None
+
+    return get_image_url(logo_picture, "2400x900", "png")
+
+
+def get_background_url(page: BeautifulSoup) -> str | None:
+    pictures = page.find_all("picture")
+    background_picture = None
+    for picture in pictures:
+        img = picture.find("img", class_="product-header__image-bg")
+        if img:
+            background_picture = picture
+            break
+
+    if not background_picture:
+        return None
+
+    return get_image_url(background_picture, "4320x3240", "jpg")
+
+
+def get_image_url(picture: BeautifulSoup, size: str, extension: str) -> str | None:
+    srcsets = [source.get("srcset") for source in picture.find_all("source")]
+    srcset = None
+    file_extension = f".{extension} "
+    for srcset in srcsets:
+        if file_extension in srcset:
+            break
+    if not srcset:
+        return None
+
+    image_url = srcset.split(", ")[0].split(" ")[0]
+    target_size = f"{size}.{extension}"
+    return get_resized_image_url(image_url, target_size)
+
+
+def get_resized_image_url(url: str, size: str) -> str:
+    """Example: size = 3840x2160.jpg"""
+    extension = size.split(".")[-1]
+    return re.sub(r"[\w]+x[\w]+\.{}$".format(extension), size, url)
