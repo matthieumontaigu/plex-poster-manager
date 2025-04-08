2b6bb4db5e867f041a029117411762e357dd00d8
matthieumontaigu <matthieu.montaigu@icloud.com>
2025-04-08 15:48:24 +0200

    Refactor get_movies and get_movie_attributes functions for improved readability and maintainability

--- client/plex/parser.py
+++ client/plex/parser.py
@@ -4,22 +4,49 @@ from xml.etree.ElementTree import Element
 from requests import Response
 
 
-def get_movies(api_response: Response) -> list[dict[str, str | list[str] | None]]:
-    root = ET.fromstring(api_response.content)
+def get_movies(
+    api_response: Response,
+) -> list[dict[str, str | list[str] | None]]:
+    root = parse_xml(api_response)
+    return _get_movies(root)
 
+
+def get_movie_attributes(
+    api_response: Response,
+) -> dict[str, str | list[str] | None]:
+    root = parse_xml(api_response)
+    movie = root.find("Video")
+    if movie is None:
+        return {}
+    return _get_movie_attributes(movie)
+
+
+def parse_xml(api_response: Response) -> Element:
+    return ET.fromstring(api_response.content)
+
+
+def _get_movies(root: Element) -> list[dict[str, str | list[str] | None]]:
     movies = []
     for movie in root.findall("Video"):
-        movie_attributes = {
-            "plex_movie_id": movie.attrib.get("ratingKey"),
-            "title": movie.attrib.get("title"),
-            "year": movie.attrib.get("year"),
-            "added_date": movie.attrib.get("addedAt"),
-            "release_date": movie.attrib.get("originallyAvailableAt"),
-            "director": get_directors(movie),
-            "tmdb_id": get_tmdb_id(movie),
-        }
+        movie_attributes = _get_movie_attributes(movie)
         movies.append(movie_attributes)
     return movies
+
+
+def _get_movie_attributes(movie: Element) -> dict[str, str | list[str] | None]:
+    """Extract movie attributes from the XML element."""
+    attributes = {
+        "plex_movie_id": movie.attrib.get("ratingKey"),
+        "title": movie.attrib.get("title"),
+        "year": movie.attrib.get("year"),
+        "added_date": movie.attrib.get("addedAt"),
+        "release_date": movie.attrib.get("originallyAvailableAt"),
+        "director": get_directors(movie),
+    }
+    tmdb_id = get_tmdb_id(movie)
+    if tmdb_id:
+        attributes["tmdb_id"] = tmdb_id
+    return attributes
 
 
 def get_directors(movie: Element) -> list[str]:
