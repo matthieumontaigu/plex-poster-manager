3572d453950b69fa826b438d08aa7131583226e8
matthieumontaigu <matthieu.montaigu@icloud.com>
2025-04-08 17:36:46 +0200

    Refactor API request methods to unify naming and improve consistency

--- client/plex/api.py
+++ client/plex/api.py
@@ -13,7 +13,7 @@ class PlexAPIRequester:
         endpoint = f"library/all"
         params = {"type": 1}
 
-        response = self.get_request(endpoint, params)
+        response = self.get(endpoint, params)
         return response
 
     def get_recently_added_movies(self) -> Response:
@@ -21,14 +21,14 @@ class PlexAPIRequester:
         endpoint = f"library/recentlyAdded"
         params = {"type": 1}
 
-        response = self.get_request(endpoint, params)
+        response = self.get(endpoint, params)
         return response
 
     def get_metadata(self, movie_id: int) -> Response:
         """Get metadata for a specific movie."""
         endpoint = f"library/metadata/{movie_id}"
 
-        response = self.get_request(endpoint, {})
+        response = self.get(endpoint, {})
         return response
 
     def upload_poster(self, movie_id: int, poster_url: str) -> bool:
@@ -36,7 +36,7 @@ class PlexAPIRequester:
         endpoint = f"library/metadata/{movie_id}/posters"
         params = {"url": poster_url}
 
-        response = self.post_request(endpoint, params)
+        response = self.post(endpoint, params)
         return response.status_code == 200
 
     def upload_background(self, movie_id: int, background_url: str) -> bool:
@@ -44,7 +44,7 @@ class PlexAPIRequester:
         endpoint = f"library/metadata/{movie_id}/arts"
         params = {"url": background_url}
 
-        response = self.post_request(endpoint, params)
+        response = self.post(endpoint, params)
         return response.status_code == 200
 
     def upload_logo(self, movie_id: int, logo_url: str) -> bool:
@@ -52,7 +52,7 @@ class PlexAPIRequester:
         endpoint = f"library/metadata/{movie_id}/clearLogos"
         params = {"url": logo_url}
 
-        response = self.post_request(endpoint, params)
+        response = self.post(endpoint, params)
         return response.status_code == 200
 
     def update_release_date(self, movie_id: int, release_date: str) -> bool:
@@ -62,22 +62,22 @@ class PlexAPIRequester:
             "originallyAvailableAt": release_date,
         }
 
-        response = self.put_request(endpoint, params)
+        response = self.put(endpoint, params)
         return response.status_code == 200
 
-    def get_request(self, endpoint: str, params: dict) -> Response:
+    def get(self, endpoint: str, params: dict) -> Response:
         url = f"{self.api_url}/{endpoint}"
         response = requests.get(url, headers=self.headers, params=params)
         response.raise_for_status()
         return response
 
-    def post_request(self, endpoint: str, params: dict) -> Response:
+    def post(self, endpoint: str, params: dict) -> Response:
         url = f"{self.api_url}/{endpoint}"
         response = requests.post(url, headers=self.headers, params=params)
         response.raise_for_status()
         return response
 
-    def put_request(self, endpoint: str, params: dict) -> Response:
+    def put(self, endpoint: str, params: dict) -> Response:
         url = f"{self.api_url}/{endpoint}"
         response = requests.put(url, headers=self.headers, params=params)
         response.raise_for_status()
