179e90a9efe6274536355735368333ade9930a30
matthieumontaigu <matthieu.montaigu@icloud.com>
2025-04-09 16:43:27 +0200

    Refactor PlexManager to improve type annotations and remove unused update_artworks method

--- client/plex/manager.py
+++ client/plex/manager.py
@@ -1,9 +1,7 @@
 import logging
-import time
 
+from client.plex.api import PlexAPIRequester
 from client.plex.parser import get_movie_attributes, get_movies
-
-from .api import PlexAPIRequester
 
 
 class PlexManager:
@@ -11,42 +9,19 @@ class PlexManager:
         self.api_requester = PlexAPIRequester(plex_url, plex_token)
         self.logger = logging.getLogger(__name__)
 
-    def get_all_movies(self) -> list[dict[str, str | None]]:
+    def get_all_movies(self) -> list[dict[str, str | int | None]]:
         api_response = self.api_requester.get_all_movies()
         if api_response is None:
             self.logger.error("Failed to fetch all movies from Plex.")
             return []
         return get_movies(api_response)
 
-    def get_recently_added_movies(self) -> list[dict[str, str | None]]:
+    def get_recently_added_movies(self) -> list[dict[str, str | int | None]]:
         api_response = self.api_requester.get_recently_added_movies()
         if api_response is None:
             self.logger.error("Failed to fetch recently added movies from Plex.")
             return []
         return get_movies(api_response)
-
-    def update_artworks(
-        self, plex_movie_id: int, poster_url: str, background_url: str, logo_url: str
-    ) -> bool:
-        """
-        Update the movie artworks (poster, background, logo) for a given Plex movie ID.
-        :param plex_movie_id: The Plex movie ID.
-        :param poster_url: The URL of the poster image.
-        :param background_url: The URL of the background image.
-        :param logo_url: The URL of the logo image.
-        :return: True if all uploads were successful, False otherwise.
-        """
-        success = True
-        if poster_url:
-            success &= self.upload_poster(plex_movie_id, poster_url)
-            time.sleep(0.5)
-        if background_url:
-            success &= self.upload_background(plex_movie_id, background_url)
-            time.sleep(0.5)
-        if logo_url:
-            success &= self.upload_logo(plex_movie_id, logo_url)
-            time.sleep(0.5)
-        return success
 
     def upload_poster(self, plex_movie_id: int, poster_url: str) -> bool:
         return self.api_requester.upload_poster(plex_movie_id, poster_url)
