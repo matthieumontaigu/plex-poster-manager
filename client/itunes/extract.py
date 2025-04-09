56bc60e747ccaedbebbfb825fe04446ee8fd974d
matthieumontaigu <matthieu.montaigu@icloud.com>
2025-04-09 17:40:21 +0200

    Update get_itunes_artworks to return None for no matches instead of empty strings

--- client/itunes/extract.py
+++ client/itunes/extract.py
@@ -7,12 +7,12 @@ from utils.string_utils import normalize
 
 def get_itunes_artworks(
     title: str, directors: list[str], year: int, country: str
-) -> tuple[str, str, str]:
+) -> tuple[str, str, str] | None:
     candidates = search_movies(country, title.lower())
 
     match = get_matching_movie(candidates, title, directors, year)
     if not match:
-        return "", "", ""
+        return None
 
     itunes_url, poster_url, release_date = get_artworks(match)
     return itunes_url, poster_url, release_date
