ffb8f0603ea7a299f02aea5bda699f7974fc6f79
matthieumontaigu <matthieu.montaigu@icloud.com>
2025-04-09 14:22:19 +0200

    Implement search_movies function to fetch movie data from iTunes API

---
+++ client/itunes/search.py
@@ -0,0 +1,14 @@
+from utils.requests_utils import get_request
+
+BASE_URL = "https://itunes.apple.com"
+
+
+def search_movies(country: str, term: str) -> list[dict]:
+    url = f"{BASE_URL}/search"
+    params = {"entity": "movie", "country": country, "term": term}
+
+    response = get_request(url, params=params)
+    response_json = response.json()
+    if response_json["resultCount"] == 0:
+        return []
+    return response_json["results"]
