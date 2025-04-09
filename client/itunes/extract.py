52f2c6b977576859d0dd8d60352a9904dcb309c1
matthieumontaigu <matthieu.montaigu@icloud.com>
2025-04-09 22:54:07 +0200

    Refactor matching logic to use are_match for title and director comparisons

--- client/itunes/extract.py
+++ client/itunes/extract.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 from client.itunes.parser import get_artworks, get_director, get_title, get_year
 from client.itunes.search import search_movies
-from utils.string_utils import normalize
+from utils.string_utils import are_match
 
 
 def get_itunes_artworks(
@@ -28,23 +28,13 @@ def get_matching_movie(
 
 
 def is_matching(candidate: dict, title: str, directors: list[str], year: int) -> bool:
-    same_title = is_title_match(title, get_title(candidate))
+    same_title = are_match(title, get_title(candidate))
     if not same_title:
         return False
 
     same_director = is_director_match(directors, get_director(candidate))
     same_year = is_year_match(year, get_year(candidate))
-    if same_director or same_year:
-        return True
-
-    return False
-
-
-def is_title_match(
-    title: str,
-    candidate_title: str,
-) -> bool:
-    return normalize(title) in normalize(candidate_title)
+    return same_director or same_year
 
 
 def is_director_match(directors: list[str], candidate_director: str) -> bool:
@@ -52,13 +42,13 @@ def is_director_match(directors: list[str], candidate_director: str) -> bool:
     directors = ["Jason Hand", " Dana Ledoux Miller", "David G. Derrick, Jr."]
     candidate_director = "Jason Hand, Dana Ledoux Miller & David G. Derrick, Jr."
     """
-    candidate_director = normalize(candidate_director)
     if not directors:
         return True
+
     for director in directors:
-        director = normalize(director)
-        if director in candidate_director:
+        if are_match(director, candidate_director):
             return True
+
     return False
 
 
@@ -66,4 +56,4 @@ def is_year_match(
     year: int,
     candidate_year: int,
 ) -> bool:
-    return year - 1 <= candidate_year <= year + 1
+    return year <= candidate_year <= year + 1
