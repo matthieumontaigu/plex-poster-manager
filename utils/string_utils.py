28a397dd0ca0efc403dfd15c53b8dba7f33194d3
matthieumontaigu <matthieu.montaigu@icloud.com>
2025-04-10 13:19:25 +0200

    Add are_match function for normalized string comparison

--- utils/string_utils.py
+++ utils/string_utils.py
@@ -2,6 +2,13 @@ import unicodedata
 from string import punctuation
 
 
+def are_match(s1: str, s2: str) -> bool:
+    """
+    Check if two strings are equal after normalization.
+    """
+    return normalize(s1) in normalize(s2)
+
+
 def normalize(s: str) -> str:
     return remove_punctuation(remove_accents(s)).lower().strip()
 
