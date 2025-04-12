644ecf08966bc113552468df7464261ea17cda98
matthieumontaigu <matthieu.montaigu@icloud.com>
2025-04-12 12:10:01 +0200

    Refactor movie matching logic for improved clarity and consistency

--- client/itunes/match.py
+++ client/itunes/match.py
@@ -1,5 +1,5 @@
 from client.itunes.parser import get_director, get_title, get_year
-from utils.string_utils import are_match
+from utils.string_utils import get_similarity, is_included
 
 
 def get_matching_movie(
@@ -20,40 +20,50 @@ def get_matching_movie(
 def compute_match_score(
     candidate: dict, title: str, directors: list[str], year: int
 ) -> float:
-    TITLE_SCORE = 1.0
-    DIRECTOR_SCORE = 0.6
-    YEAR_SCORE = 0.6
+    TITLE_WEIGHT = 1.0
+    DIRECTOR_WEIGHT = 0.6
+    YEAR_WEIGHT = 0.6
 
     score = 0.0
-    same_title = are_match(title, get_title(candidate))
-    same_director = is_director_match(directors, get_director(candidate))
+    same_title = is_title_match(title, get_title(candidate))
+    director_score = get_director_score(directors, get_director(candidate))
     same_year = is_year_match(year, get_year(candidate))
 
     if same_title:
-        score += TITLE_SCORE
+        score += TITLE_WEIGHT
 
-    if same_director:
-        score += DIRECTOR_SCORE
+    if director_score:
+        score += director_score * DIRECTOR_WEIGHT
 
     if same_year:
-        score += YEAR_SCORE
+        score += YEAR_WEIGHT
 
     return score
 
 
-def is_director_match(directors: list[str], candidate_director: str) -> bool:
+def is_title_match(
+    title: str,
+    candidate_title: str,
+) -> bool:
+    similarity = get_similarity(title, candidate_title)
+    return similarity >= 0.9
+
+
+def get_director_score(directors: list[str], candidate_director: str) -> float:
     """
     directors = ["Jason Hand", " Dana Ledoux Miller", "David G. Derrick, Jr."]
     candidate_director = "Jason Hand, Dana Ledoux Miller & David G. Derrick, Jr."
     """
-    if not directors:
-        return True
+    if not directors or candidate_director in ("", "Unknown"):
+        return 0.0
 
     for director in directors:
-        if are_match(director, candidate_director):
-            return True
+        if is_included(director, candidate_director):
+            return 1.0
 
-    return False
+    director = "".join(directors)
+    similarity = get_similarity(director, candidate_director)
+    return -1.0 if similarity < 0.5 else 0.0
 
 
 def is_year_match(
