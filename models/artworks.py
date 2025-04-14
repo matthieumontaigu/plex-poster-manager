4705f264f7816cc35a0584f0a63661907b49ffcf
matthieumontaigu <matthieu.montaigu@icloud.com>
2025-04-14 19:58:24 +0200

    Enhance Artworks update method to include source and title, and improve logo and poster matching logic

--- models/artworks.py
+++ models/artworks.py
@@ -23,14 +23,24 @@ class Artworks:
     def is_missing(self, key: str) -> bool:
         return not self.data[key]
 
-    def update(self, key: str, url: str, country: str, title: str) -> bool:
-        if not url or self.data[key]:
+    def update(
+        self,
+        key: str,
+        url: str,
+        country: str,
+        title: str,
+        source: str = "",
+        override: bool = False,
+    ) -> bool:
+        if not url or (self.data[key] and not override):
             return False
 
         if key in self._texted_artworks and not self.can_update_texted(title):
             return False
 
-        self.data[key] = {"url": url, "country": country}
+        self.data[key] = {"url": url, "country": country, "title": title}
+        if source:
+            self.data[key]["source"] = source
         return True
 
     def should_handle(self, title: str) -> bool:
@@ -47,6 +57,17 @@ class Artworks:
             return True
 
         return are_match(title, self.movie_title)
+
+    def logo_matches_poster(self) -> bool:
+        poster_title = self.data["poster"].get("title")
+        logo_title = self.data["logo"].get("title")
+        if not poster_title or not logo_title:
+            return True
+
+        return are_match(poster_title, logo_title)
+
+    def get_poster_country(self) -> str | None:
+        return self.data["poster"].get("country")
 
     def _is_any_textless_artwork_missing(self) -> bool:
         return any(self.is_missing(key) for key in self._textless_artworks)
