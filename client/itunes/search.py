296494de87f189bc8451b7fd70975169d9adf7be
matthieumontaigu <matthieu.montaigu@icloud.com>
2025-10-05 17:20:44 +0200

    Refactor search_movies to filter results for feature movies and update documentation

--- client/itunes/search.py
+++ client/itunes/search.py
@@ -1,3 +1,4 @@
+from client.itunes.parser import get_kind
 from utils.requests_utils import get_request
 from utils.string_utils import soft_normalize
 
@@ -5,9 +6,16 @@ BASE_URL = "https://itunes.apple.com"
 
 
 def search_movies(country: str, term: str) -> list[dict]:
+    """
+    Search for movies on iTunes.
+    Available parameters described here:
+    https://performance-partners.apple.com/search-api
+
+    As of 2025-10-06, the "entity" parameter seems to be ignored for movie searches.
+    """
     url = f"{BASE_URL}/search"
     term = soft_normalize(term)
-    params = {"entity": "movie", "country": country, "term": term}
+    params = {"country": country, "term": term}
 
     response = get_request(url, params=params)
     if response is None:
@@ -15,4 +23,11 @@ def search_movies(country: str, term: str) -> list[dict]:
     response_json = response.json()
     if response_json["resultCount"] == 0:
         return []
-    return response_json["results"]
+
+    # Filter out non-movie results
+    results = [
+        result
+        for result in response_json["results"]
+        if get_kind(result) == "feature-movie"
+    ]
+    return results
