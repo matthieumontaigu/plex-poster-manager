e21807bd68508be92997b7ad07de1e9ab3f3001c
matthieumontaigu <matthieu.montaigu@icloud.com>
2025-10-06 20:31:33 +0200

    Add LANGUAGE_CODES mapping for country codes to TMDB language codes

---
+++ models/countries.py
@@ -0,0 +1,16 @@
+# Mapping between country codes and TMDB language codes
+
+LANGUAGE_CODES = {
+    "fr": "fr",
+    "us": "en",
+    "gb": "en",
+    "au": "en",
+    "nz": "en",
+    "ca": "fr",
+    "be": "fr",
+    "lu": "fr",
+    "ch": "fr",
+    "de": "de",
+    "it": "it",
+    "es": "es",
+}
