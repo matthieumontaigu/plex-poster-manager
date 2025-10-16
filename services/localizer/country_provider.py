279f7f585885d3cee0c9044cf7cb08cf7d73b751
matthieumontaigu <matthieu.montaigu@icloud.com>
2025-10-16 19:44:49 +0200

    Refactor CountryProvider to remove metadata handling and streamline artwork retrieval

--- services/localizer/country_provider.py
+++ services/localizer/country_provider.py
@@ -2,10 +2,10 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from models.artworks import build_image, build_metadata
+from models.artworks import build_image
 
 if TYPE_CHECKING:
-    from models.artworks import Image, Metadata
+    from models.artworks import Image
     from models.movie import Movie
     from services.localizer.localizer import Localizer
     from services.provider.base import Provider
@@ -25,22 +25,23 @@ class CountryProvider:
         }
 
     def get_artworks(
-        self, movie: Movie, localized_title: str
-    ) -> tuple[Image | None, Image | None, Image | None, Metadata | None]:
-        search_args = (localized_title, movie["director"], movie["year"], self.country)
+        self, movie: Movie
+    ) -> tuple[Image | None, Image | None, Image | None]:
+
+        localized_title = self.localizer.get_localized_title(movie, self.country)
+        if not localized_title:
+            return None, None, None
 
         base_image = {"title": localized_title, **self.base_image}
 
+        search_args = (localized_title, movie["director"], movie["year"], self.country)
         poster_url, background_url, logo_url = self.provider.get_artworks(*search_args)
 
         poster = build_image(poster_url, **base_image)
         background = build_image(background_url, **base_image)
         logo = build_image(logo_url, **base_image)
 
-        release_date_str = self.provider.get_release_date(*search_args)
-        release_date = build_metadata(release_date_str, self.country)
-
-        return poster, background, logo, release_date
+        return poster, background, logo
 
     def get_localized_title(self, movie: Movie) -> str | None:
         return self.localizer.get_localized_title(movie, self.country)
