bece977c4c6407e51a2a7add63a0cac6e2d56b7e
matthieumontaigu <matthieu.montaigu@icloud.com>
2025-10-20 20:12:49 +0200

    Refactor ArtworksUpdater to include ArtworksSelector for selecting artworks before upload

--- services/artworks/updater.py
+++ services/artworks/updater.py
@@ -7,6 +7,7 @@ if TYPE_CHECKING:
     from models.artworks import Artworks
     from models.movie import Movie
     from services.artworks.retriever import ArtworksRetriever
+    from services.artworks.selector import ArtworksSelector
     from services.artworks.uploader import ArtworksUploader
 
 logger = logging.getLogger(__name__)
@@ -16,14 +17,17 @@ class ArtworksUpdater:
     """Coordinates the process of retrieving, matching, and uploading artworks."""
 
     def __init__(
-        self, artworks_retriever: ArtworksRetriever, artworks_uploader: ArtworksUploader
+        self,
+        artworks_retriever: ArtworksRetriever,
+        artworks_uploader: ArtworksUploader,
+        artworks_selector: ArtworksSelector,
     ) -> None:
         self.retriever = artworks_retriever
         self.uploader = artworks_uploader
+        self.selector = artworks_selector
 
     def update(self, movie: Movie) -> tuple[Artworks, bool]:
         artworks = self.retriever.retrieve(movie)
-
-        uploaded = self.uploader.upload(movie, artworks)
-
+        selected_artworks = self.selector.select(artworks, movie)
+        uploaded = self.uploader.upload(movie, selected_artworks)
         return artworks, uploaded
