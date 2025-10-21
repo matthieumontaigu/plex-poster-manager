80169af05d5b78c343bf1b4de3947bbd1a4dc59a
matthieumontaigu <matthieu.montaigu@icloud.com>
2025-10-21 12:51:23 +0200

    Add method to retrieve movie release dates by country

--- client/tmdb/api.py
+++ client/tmdb/api.py
@@ -3,6 +3,8 @@ import logging
 import requests
 from requests.models import Response
 
+from client.tmdb.parser import get_country_release_date
+
 logger = logging.getLogger(__name__)
 
 
@@ -16,6 +18,17 @@ class TMDBAPIRequester:
     def get_movie_title(self, movie_id: int, language: str) -> str | None:
         movie_details = self.get_movie_details(movie_id, language)
         return movie_details.get("title")
+
+    def get_release_date(self, movie_id: int, country_code: str) -> str | None:
+        release_dates_info = self.get_release_dates(movie_id)
+        results = release_dates_info.get("results", [])
+        return get_country_release_date(results, country_code)
+
+    def get_release_dates(self, movie_id: int) -> dict:
+        endpoint = f"movie/{movie_id}/release_dates"
+        params = {"api_key": self.api_token}
+        response = self.get(endpoint, params=params)
+        return response.json() if response else {}
 
     def get_movie_details(self, movie_id: int, language: str) -> dict:
         endpoint = f"movie/{movie_id}"
