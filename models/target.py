ff93a82cedc1519ef64291a9faa8dadc23acae1c
matthieumontaigu <matthieu.montaigu@icloud.com>
2025-11-13 16:11:58 +0800

    Add Target dataclass to define movie/show attributes

---
+++ models/target.py
@@ -0,0 +1,11 @@
+from dataclasses import dataclass
+
+
+@dataclass(slots=True)
+class Target:
+    title: str
+    directors: list[str]
+    year: int
+    country: str
+    entity: str  # "movie" | "show"
+    entity: str  # "movie" | "show"
